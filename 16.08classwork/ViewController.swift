b132a042d57c517b24d6e3dcbb6a38011a182173
Aker <@akerke.n@inbox.ru>
2023-08-16 20:17:54 +0600

    Initial Commit

---
+++ 16.08classwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  16.08classwork
+//
+//  Created by Akerke on 16.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
